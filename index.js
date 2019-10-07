6ea086c1e75234894ffee8d880e5dd747614b7b4
Sibbons Shrestha <Sibbonsss@gmail.com>
2019-10-06 17:31:31 -0700

    added heroku in index.js

--- index.js
+++ index.js
@@ -7,4 +7,18 @@ app.use(bodyParser.json());
 app.use(cors());
 require('./routes/randomPicker')(app);
 
-app.listen(4000);
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve production asset
+    app.use(express.static('client/build'));
+    //Express will serve index.html if it doesnt rezonize route
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT);
