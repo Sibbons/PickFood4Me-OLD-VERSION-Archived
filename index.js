80352886ead5197f46c13b06c7db289b36e39945
Sibbons Shrestha <SibbonsSS@gmail.com>
2019-10-18 10:52:50 -0700

    heroku redirect

--- index.js
+++ index.js
@@ -2,27 +2,20 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const sslRedirect = require('heroku-ssl-redirect');
 
 app.use(bodyParser.json());
 app.use(cors());
 require('./routes/randomPicker')(app);
 
 if (process.env.NODE_ENV === 'production') {
-    // Express will serve production asset
-    app.use(express.static('client/build'));
-    //Express will serve index.html if it doesnt rezonize route
-    app.use((req, res, next) => {
-        if (req.header('x-forwarded-proto') !== 'https')
-            res.redirect(`https://${req.header('host')}${req.url}`)
-        else
-            next()
-    })
-
     const path = require('path');
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
+app.use(sslRedirect());
+
 
 const PORT = process.env.PORT || 4000;
 
